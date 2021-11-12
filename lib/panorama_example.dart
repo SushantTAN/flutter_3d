b1c90a04dd74b5e18212d3ab5b6b361de49b0a3e
SushantTAN <punk1susant@gmail.com>
2021-11-12 12:42:29 +0545

    panorama

--- lib/panorama_example.dart
+++ lib/panorama_example.dart
@@ -20,11 +20,20 @@ class _PanoramicState extends State<Panoramic> {
         title: Text("Panoramic"),
       ),
       body: Center(child: 
-        Panorama( 
-          animSpeed: 0.0,
-          child: Image.network('https://l13.alamy.com/360/2BWEJ0M/full-spherical-seamless-hdri-panorama-360-degrees-in-the-yard-near-orthodox-church-in-equirectangular-projection-with-zenith-and-nadir-vr-content-2BWEJ0M.jpg'),
-          // child: Image.asset('assets/images/PANO_20211104_183314.jpg'),
-         )
+        Stack(
+          // http://apps.laticrete.me/media/static/p_3_revise.png
+          children: <Widget>[
+            Container(
+             decoration: BoxDecoration(color: Colors.amberAccent),
+           ),
+            Panorama( 
+            animSpeed: 0.0,
+            child: Image.network('https://l13.alamy.com/360/2BWEJ0M/full-spherical-seamless-hdri-panorama-360-degrees-in-the-yard-near-orthodox-church-in-equirectangular-projection-with-zenith-and-nadir-vr-content-2BWEJ0M.jpg'),
+            // child: Image.asset('assets/images/PANO_20211104_183314.jpg'),
+           ),
+           
+           ],
+        )
       ),
     );
   }
